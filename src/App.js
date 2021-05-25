cb91d9e68df26283fcbd91e19acd97b20a820211
Joe McCleary <joemccleary@hotmail.co.uk>
2021-05-25 14:52:04 +0100

    navigation bar added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, {useState} from 'react'
+import React, {Component, useState} from 'react'
 import Accordion from './components/Accordion'
 import Search from './components/Search'
 import Dropdown from './components/dropdown'
 import Translate from './components/translate'
+import Route from './Route'
+import Header from './components/header'
 
 const items = [
     {
@@ -34,11 +36,57 @@ const options = [
     }
 ];
 
-export default () => {
+const showAccordion = () => {
+    if (window.location.pathname === '/') {
+        return <Accordion items={items} />;
+    }
+};
+const showList = () => {
+    if (window.location.pathname === '/list') {
+        return <Search />;
+    }
+};
+const showDropdown = () => {
+    if (window.location.pathname === '/dropdown') {
+        return <Dropdown />;
+    }
+};
+const showTranslate = () => {
+    if (window.location.pathname === '/translate') {
+        return <Translate />;
+    }
+};
 
+const showComponent = (route, component) => {
+    return window.location.pathname === route
+    ? component 
+    : null;
+}
+
+export default () => {
+    const [selected, setSelected] = useState();
     return ( 
         <div>
-            <Translate />
+            <Header />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown 
+                    label="selecta color"
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                    />
+            </Route>
+            <Route path="/translate">
+                <Translate/>
+            </Route>
+
+
         </div>
     );
 };
